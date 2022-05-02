ebfb91b790c95a36c19bf568781d0b14fae1eda5
Gerd Aschemann <gerd@aschemann.net>
2022-05-02 08:55:25 +0200

    Migrate "aws_ec2_ebs_docker_host" to current Terraform (>= 1.0)

--- aws/aws_ec2_ebs_docker_host/main.tf
+++ aws/aws_ec2_ebs_docker_host/main.tf
@@ -1,24 +1,29 @@
 # Create the main EC2 instance
 # https://www.terraform.io/docs/providers/aws/r/instance.html
 resource "aws_instance" "this" {
-  instance_type          = "${var.instance_type}"
-  ami                    = "${var.instance_ami}"
-  availability_zone      = "${local.availability_zone}"
-  key_name               = "${aws_key_pair.this.id}"                            # the name of the SSH keypair to use for provisioning
+  instance_type          = var.instance_type
+  ami                    = var.instance_ami
+  availability_zone      = local.availability_zone
+  key_name               = aws_key_pair.this.id # the name of the SSH keypair to use for provisioning
   vpc_security_group_ids = ["${aws_security_group.this.id}"]
-  subnet_id              = "${data.aws_subnet.this.id}"
-  user_data              = "${sha1(local.reprovision_trigger)}"                 # this value isn't used by the EC2 instance, but its change will trigger re-creation of the resource
-  tags                   = "${merge(var.tags, map("Name", "${var.hostname}"))}"
-  volume_tags            = "${merge(var.tags, map("Name", "${var.hostname}"))}" # give the root EBS volume a name (+ other possible tags) that makes it easier to identify as belonging to this host
+  subnet_id              = data.aws_subnet.this.id
+  user_data              = sha1(local.reprovision_trigger)
+  # this value isn't used by the EC2 instance, but its change will trigger re-creation of the resource
+  tags        = merge(var.tags, { Name = var.hostname })
+  volume_tags = merge(var.tags, { Name = var.hostname })
+  # give the root EBS volume a name (+ other possible tags) that makes it easier to identify as belonging to this host
 
   root_block_device {
-    volume_size = "${var.root_volume_size}"
+    volume_size = var.root_volume_size
   }
 
   connection {
-    user        = "${var.ssh_username}"
-    private_key = "${file("${var.ssh_private_key_path}")}"
-    agent       = false                                    # don't use SSH agent because we have the private key right here
+    type = "ssh"
+    host = var.hostname
+    user = var.ssh_username
+    #    private_key = file("${var.ssh_private_key_path}")
+    agent          = true
+    agent_identity = var.ssh_agent_identity
   }
 
   provisioner "remote-exec" {
@@ -46,33 +51,43 @@ resource "aws_instance" "this" {
 }
 
 # Attach the separate data volume to the instance, if so configured
-
 resource "aws_volume_attachment" "this" {
-  count       = "${var.data_volume_id == "" ? 0 : 1}" # only create this resource if an external EBS data volume was provided
-  device_name = "/dev/xvdh"                           # note: this depends on the AMI, and can't be arbitrarily changed
-  instance_id = "${aws_instance.this.id}"
-  volume_id   = "${var.data_volume_id}"
+  count       = var.data_volume_id == "" ? 0 : 1 # only create this resource if an external EBS data volume was provided
+  device_name = "/dev/xvdh"                      # note: this depends on the AMI, and can't be arbitrarily changed
+  instance_id = aws_instance.this.id
+  volume_id   = var.data_volume_id
 }
 
 resource "null_resource" "provisioners" {
-  count      = "${var.data_volume_id == "" ? 0 : 1}" # only create this resource if an external EBS data volume was provided
-  depends_on = ["aws_volume_attachment.this"]        # because we depend on the EBS volume being available
+  count      = var.data_volume_id == "" ? 0 : 1 # only create this resource if an external EBS data volume was provided
+  depends_on = [aws_volume_attachment.this]     # because we depend on the EBS volume being available
+
+  triggers = {
+    agent_identiy = var.ssh_agent_identity
+    public_ip     = aws_instance.this.public_ip
+    user          = var.ssh_username
+    #    private_key = file(var.ssh_private_key_path)
+    device_name = aws_volume_attachment.this[0].device_name
+  }
 
   connection {
-    host        = "${aws_instance.this.public_ip}"
-    user        = "${var.ssh_username}"
-    private_key = "${file("${var.ssh_private_key_path}")}"
-    agent       = false                                    # don't use SSH agent because we have the private key right here
+    type = "ssh"
+    host = self.triggers.public_ip
+    user = self.triggers.user
+    #    private_key = self.triggers.private_key
+    agent          = true
+    agent_identity = self.triggers.agent_identiy
   }
 
   # When creating the attachment
   provisioner "remote-exec" {
+    when   = create
     script = "${path.module}/provision-ebs.sh"
   }
 
   # When tearing down the attachment
   provisioner "remote-exec" {
-    when   = "destroy"
-    inline = ["sudo umount -v ${aws_volume_attachment.this.device_name}"]
+    when   = destroy
+    inline = ["sudo umount -v ${self.triggers.device_name}"]
   }
 }
